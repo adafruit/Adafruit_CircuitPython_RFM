b4ab57e44fb1472ea1dbe14e8deac45af34ce2a0
jerryneedell <jerryneedell@gmail.com>
2024-08-01 07:12:00 -0400

    more example revisions

--- examples/rfm_fsk_node1.py
+++ examples/rfm_fsk_node1.py
@@ -9,8 +9,6 @@ import board
 import busio
 import digitalio
 
-from adafruit_rfm import rfm69
-
 # Define radio parameters.
 RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
 # module! Can be a value like 915.0, 433.0, etc.
@@ -23,26 +21,47 @@ RESET = digitalio.DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
 # Initialze RFM radio
-rfm69 = rfm69.RFM69(spi, CS, RESET, RADIO_FREQ_MHZ)
+# uncommnet the desired import and rfm initialization depending on the radio boards being used
+
+# Use rfm9x for two RFM9x radios using LoRa
+
+# from adafruit_rfm import rfm9x
+
+# rfm = rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+# Use rfm9xfsk for two RFM9x radios or RFM9x to RFM69 using FSK
+
+from adafruit_rfm import rfm9xfsk
+
+rfm = rfm9xfsk.RFM9xFSK(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+# Use rfm69 for two RFM69 radios using FSK
+
+# from adafruit_rfm import rfm69
+
+# rfm = rfm69.RFM69(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+# Disable the RadioHead  Header
+rfm.radiohead = False
 # set the time interval (seconds) for sending packets
 transmit_interval = 5
 node = 1
 destination = 2
-rfm69.radiohead = False
-rfm69.enable_address_filter = True
-rfm69.fsk_node_address = node
-rfm69.fsk_broadcast_address = 0xFF
+rfm.enable_address_filter = True
+rfm.fsk_node_address = node
+rfm.fsk_broadcast_address = 0xFF
+# For RFM69 only:
 # Optionally set an encryption key (16 byte AES key). MUST match both
 # on the transmitter and receiver (or be set to None to disable/the default).
-rfm69.encryption_key = None
-# rfm69.encryption_key = (
+rfm.encryption_key = None
+# rfm.encryption_key = (
 #    b"\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08"
 # )
 
 # initialize counter
 counter = 0
 # send a broadcast mesage
-rfm69.send(bytes(f"message number {counter}", "UTF-8"), destination=destination)
+rfm.send(bytes(f"message number {counter}", "UTF-8"), destination=destination)
 
 # Wait to receive packets.
 print("Waiting for packets...")
@@ -50,7 +69,7 @@ print("Waiting for packets...")
 time_now = time.monotonic()
 while True:
     # Look for a new packet - wait up to 5 seconds:
-    packet = rfm69.receive(timeout=2.0)
+    packet = rfm.receive(timeout=2.0)
     # If no packet was received during the timeout then None is returned.
     if packet is not None:
         # Received a packet!
@@ -61,4 +80,4 @@ while True:
         # reset timeer
         time_now = time.monotonic()
         counter = counter + 1
-        rfm69.send(bytes(f"message number {counter}", "UTF-8"), destination=destination)
+        rfm.send(bytes(f"message number {counter}", "UTF-8"), destination=destination)
--- examples/rfm_fsk_node2.py
+++ examples/rfm_fsk_node2.py
@@ -1,16 +1,13 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-FileCopyrightText: 2020 Jerry Needell for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
 # Example to send a packet periodically
-# Author: Jerry Needell
-#
+
 import time
 
 import board
 import busio
 import digitalio
-
-from adafruit_rfm import rfm69
 
 # Define radio parameters.
 RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
@@ -23,47 +20,56 @@ RESET = digitalio.DigitalInOut(board.D25)
 # Initialize SPI bus.
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-
 # Initialze RFM radio
-rfm69 = rfm69.RFM69(spi, CS, RESET, RADIO_FREQ_MHZ)
-# set the time interval (seconds) for sending packets
-node = 1
-destination = 2
-rfm69.radiohead = False
-print(rfm69.modulation_type)
-rfm69.modulation_type = 1
-print(rfm69.modulation_type)
-# Optionally set an encryption key (16 byte AES key). MUST match both
-# on the transmitter and receiver (or be set to None to disable/the default).
-rfm69.encryption_key = None
-# rfm69.encryption_key = (
-#    b"\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08"
-# )
+# uncommnet the desired import and rfm initialization depending on the radio boards being used
 
+# Use rfm9x for two RFM9x radios using LoRa
+
+# from adafruit_rfm import rfm9x
+
+# rfm = rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+# Use rfm9xfsk for two RFM9x radios or RFM9x to RFM69 using FSK
+
+from adafruit_rfm import rfm9xfsk
+
+rfm = rfm9xfsk.RFM9xFSK(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+# Use rfm69 for two RFM69 radios using FSK
+
+# from adafruit_rfm import rfm69
+
+# rfm = rfm69.RFM69(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+# Disable the RadioHead  Header
+rfm.radiohead = False
 # set the time interval (seconds) for sending packets
 transmit_interval = 5
-
-# Note that the radio is configured in LoRa mode so you can't control sync
-# word, encryption, frequency deviation, or other settings!
-
-# You can however adjust the transmit power (in dB).  The default is 13 dB but
-# high power radios like the RFM69 can go up to 20 dB:
-# rfm69.tx_power = 20
-
+node = 2
+destination = 1
+rfm.enable_address_filter = True
+rfm.fsk_node_address = node
+rfm.fsk_broadcast_address = 0xFF
+# For RFM69 only:
+# Optionally set an encryption key (16 byte AES key). MUST match both
+# on the transmitter and receiver (or be set to None to disable/the default).
+rfm.encryption_key = None
+# rfm.encryption_key = (
+#    b"\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08"
+# )
 
 # initialize counter
 counter = 0
 # send a broadcast mesage
-rfm69.send(bytes(f"message number {counter}", "UTF-8"))
+rfm.send(bytes(f"message number {counter}", "UTF-8"), destination=destination)
 
 # Wait to receive packets.
 print("Waiting for packets...")
 # initialize flag and timer
-send_reading = False
 time_now = time.monotonic()
 while True:
-    # Look for a new packet - wait up to 2 seconds:
-    packet = rfm69.receive(timeout=2.0)
+    # Look for a new packet - wait up to 5 seconds:
+    packet = rfm.receive(timeout=2.0)
     # If no packet was received during the timeout then None is returned.
     if packet is not None:
         # Received a packet!
@@ -73,7 +79,5 @@ while True:
     if time.monotonic() - time_now > transmit_interval:
         # reset timeer
         time_now = time.monotonic()
-        # clear flag to send data
-        send_reading = False
         counter = counter + 1
-        rfm69.send(bytes(f"message number {counter}", "UTF-8"))
+        rfm.send(bytes(f"message number {counter}", "UTF-8"), destination=destination)
